c3f5078d8250ec95e09864bf1ba24bffffc6c389
seongdeokKim <ystjdejr@gmail.com>
2022-01-09 00:04:31 +0900

    Update lda.py

--- lda.py
+++ lda.py
@@ -8,19 +8,26 @@ from utils.pipeline import Pipeline
 
 class LDAModal:
     def __init__(self, min_cf,
-                 remove_top_words,
-                 topic_number):
+                 remove_top_n_words,
+                 topic_number,
+                 top_n_words,
+                 top_n_topic_labels):
+
+        self.min_cf = min_cf
+        self.remove_top_n_words = remove_top_n_words
+        self.topic_number = topic_number
+        self.top_n_words = top_n_words
+        self.top_n_topic_labels = top_n_topic_labels
 
         self.mdl = tp.LDAModel(tw=tp.TermWeight.ONE,
-                               min_cf=min_cf,
-                               rm_top=remove_top_words,
-                               k=topic_number)
+                               min_cf=self.min_cf,
+                               rm_top=self.top_n_topic_labels,
+                               k=self.topic_number)
 
     def execute_lda(self, text_data,
-                    top_n_words,
-                    save_path,
                     topic_dist_per_doc_path,
-                    words_per_topic_path):
+                    words_per_topic_path,
+                    save_path):
 
         docs, corpus = text_data
         for doc in docs:
@@ -28,10 +35,10 @@ class LDAModal:
 
         self.train(save_path)
         self.extract_topic_dist_per_doc(topic_dist_per_doc_path)
-        self.rank_top_words_per_topic(top_n_words, words_per_topic_path)
+        self.rank_top_words_per_topic(words_per_topic_path)
 
 
-    def train(self, model_path):
+    def train(self, save_path=None):
         """LDA model training"""
 
         self.mdl.burn_in = 100
@@ -44,8 +51,9 @@ class LDAModal:
             self.mdl.train(10)
             print('Iteration: {}\tLog-likelihood: {}'.format(i, self.mdl.ll_per_word))
 
-        print('Saving...', file=sys.stderr, flush=True)
-        self.mdl.save(model_path, True)
+        if save_path is not None:
+            print('Saving...', file=sys.stderr, flush=True)
+            self.mdl.save(model_path, True)
 
     def extract_topic_dist_per_doc(self, topic_dist_per_doc_path):
         """extract topic distribution of each document"""
@@ -64,7 +72,7 @@ class LDAModal:
 
                 fw.write(f'{corpus[i]}\t{top_words}\t{topic_dist}\n')
 
-    def rank_top_words_per_topic(self, top_n_words, words_per_topic_path):
+    def rank_top_words_per_topic(self, words_per_topic_path):
         """extract probable words per topic"""
 
         # extract candidates for auto topic labeling
@@ -80,10 +88,10 @@ class LDAModal:
             labeler = tp.label.FoRelevance(self.mdl, cands, min_df=5, smoothing=1e-2, mu=0.25)
 
             for k in range(self.mdl.k):
-                labels = ', '.join(label for label, score in labeler.get_topic_labels(k, top_n=20))
+                labels = ', '.join(label for label, score in labeler.get_topic_labels(k, top_n=self.top_n_topic_labels))
                 fw.write(f'### {k}\t{labels}\n')
 
-                for word, prob in self.mdl.get_topic_words(k, top_n=top_n_words):
+                for word, prob in self.mdl.get_topic_words(k, top_n=self.top_n_words):
                     fw.write(f'{word}\t{prob}\n')
 
     def infer_unseen_document(self, saved_model, unseen_words):
@@ -97,21 +105,75 @@ class LDAModal:
         print("Log-likelihood of inference: ", ll)
         print()
 
+    def get_coherence(self):
+        # calculate coherence of each scheme
+
+        coherence_list = []
+        for type in ('u_mass', 'c_uci', 'c_npmi', 'c_v'):
+            coh = tp.coherence.Coherence(self.mdl, coherence=type, top_n=self.top_n_words)
+
+            avg_coherence = coh.get_score()
+            avg_coherence = str(avg_coherence)[:9]
+
+            coherence_per_topic = [coh.get_score(topic_id=k) for k in range(self.mdl.k)]
+            coherence_per_topic = [str(e)[:9] for e in coherence_per_topic]
+            coherence_per_topic = ", ".join(coherence_per_topic)
+
+            print('==== Coherence : {} ===='.format(type))
+            print('Average:', avg_coherence, '\nPer Topic:', coherence_per_topic)
+
+            coherence = Coherence(scheme=type,
+                                  topic_number=self.topic_number,
+                                  average_coherence=avg_coherence,
+                                  coherence_of_each_topic=coherence_per_topic)
+
+            coherence_list.append(coherence)
+
+        return coherence_list
+
+class Coherence:
+    def __init__(self, scheme,
+                 topic_number,
+                 average_coherence,
+                 coherence_of_each_topic):
+
+        self._scheme = scheme
+        self._topic_number = topic_number
+        self._average_coherence = average_coherence
+        self._coherence_of_each_topic = coherence_of_each_topic
+
+    @property
+    def scheme(self):
+        return self._scheme
+
+    @property
+    def topic_number(self):
+        return self._topic_number
+
+    @property
+    def average_coherence(self):
+        return self._average_coherence
+
+    @property
+    def coherence_of_each_topic(self):
+        return self._coherence_of_each_topic
+
 
 if __name__ == '__main__':
 
     start = time.time()  # 시작 시간 저장
 
-    topic_number = 10
-    save_path = './lda/lda.bin'
-    top_n_words = 20
+    number_of_topic = 10
+    model_path = './lda/lda.bin'
+    top_k_words = 20
     min_collection_frequency = 1
-    remove_top_words = 5
+    remove_top_k_words = 5
+    top_k_topic_labels = 5
 
     input_file = './data/abstract.txt'
 
-    words_per_topic = './lda/words_per_topic_k_' + str(topic_number) + '.txt'
-    topic_dist_per_doc = './lda/topic_distribution_per_doc_k_' + str(topic_number) + '.txt'
+    words_per_topic = './lda/words_per_topic_k_' + str(number_of_topic) + '.txt'
+    topic_dist_per_doc = './lda/topic_distribution_per_doc_k_' + str(number_of_topic) + '.txt'
 
 
     # load documents on memory
@@ -135,18 +197,19 @@ if __name__ == '__main__':
 
 
     lda_model = LDAModal(min_cf=min_collection_frequency,
-                         remove_top_words=remove_top_words,
-                         topic_number=topic_number)
+                         remove_top_n_words=remove_top_k_words,
+                         topic_number=number_of_topic,
+                         top_n_words=top_k_words,
+                         top_n_topic_labels=top_k_topic_labels)
 
     lda_model.execute_lda(text_data=(docs, corpus),
-                          top_n_words=top_n_words,
-                          save_path=save_path,
                           topic_dist_per_doc_path=topic_dist_per_doc,
-                          words_per_topic_path=words_per_topic)
+                          words_per_topic_path=words_per_topic,
+                          save_path=model_path)
 
     # test
     unseen_text='아사이 베리 블루베리 비슷하다'
-    lda_model.infer_unseen_document(saved_model=save_path,
+    lda_model.infer_unseen_document(saved_model=model_path,
                                     unseen_words=unseen_text)
 
 
