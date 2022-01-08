948e9f5b2266101e653b1125fcffc0b3f080296e
seongdeokKim <ystjdejr@gmail.com>
2022-01-09 00:05:30 +0900

    Update coherence.py

--- coherence.py
+++ coherence.py
@@ -3,57 +3,96 @@ import sys
 import tomotopy as tp
 
 from utils.pipeline import Pipeline
+from lda import LDAModal
+
+
+class ModelConfig:
+    def __init__(self, min_cf,
+                 remove_top_n_words,
+                 top_n_words,
+                 top_n_topic_labels):
+
+        self._min_cf = min_cf
+        self._remove_top_n_words = remove_top_n_words
+        self._top_n_words = top_n_words
+        self._top_n_topic_labels = top_n_topic_labels
+        self._topic_number = None
+
+    @property
+    def min_cf(self):
+        return self._min_cf
+
+    @property
+    def remove_top_n_words(self):
+        return self._remove_top_n_words
+
+    @property
+    def top_n_words(self):
+        return self._top_n_words
+
+    @property
+    def top_n_topic_labels(self):
+        return self._top_n_topic_labels
+
+    @property
+    def topic_number(self):
+        return self._topic_number
+
+    @topic_number.setter
+    def topic_number(self, value: int):
+        if value < 0 or value > 100:
+            raise ValueError("Check current_topic_number argument! something might be wrong.")
+        self._topic_number = value
 
 
 def calculate_coherence(text_data,
                         max_topic_number,
-                        top_n_words,
-                        coherence_path):
-    """calculate coherence based on four type scheme"""
+                        coherence_path,
+                        model_config):
+    """calculate coherence based on four scheme type"""
 
+    # iterate calculation as the number of topic increases
+    coherence_list = []
+    for current_topic_number in range(1, max_topic_number):
+        model_config.topic_number = current_topic_number
+
+        # declare LDAModel class
+        lda_model = LDAModal(min_cf=model_config.min_cf,
+                             remove_top_n_words=model_config.remove_top_n_words,
+                             top_n_words=model_config.top_n_words,
+                             top_n_topic_labels=model_config.top_n_topic_labels,
+                             topic_number=model_config.topic_number)
+
+        # put documents to lda_model instance
+        for doc in text_data:
+            lda_model.mdl.add_doc(doc)
+
+        # model train
+        lda_model.train()
+
+        # calculate coherence of each scheme
+        coherences = lda_model.get_coherence()
+        coherence_list += coherences
+
+    # file writer
     with open(coherence_path, 'w', encoding='utf-8') as fw:
-
-        # iterate calculation as number of topic increases
-        fw.write('scheme_type\ttopic_number\tavg coherence\tcoherence_dist\n')
-        for topic_number in range(max_topic_number):
-            # declare LDAModel class
-            mdl = tp.LDAModel(tw=tp.TermWeight.ONE, min_cf=1, rm_top=5, k=topic_number+1)
-            for doc in text_data:
-                mdl.add_doc(doc)
-
-            # model train
-            mdl.burn_in = 100
-            mdl.train(0)
-            print('Num docs:', len(mdl.docs), ', Vocab size:', mdl.num_vocabs, ', Num words:', mdl.num_words)
-            print('Removed top words:', mdl.removed_top_words)
-            print('Training...', file=sys.stderr, flush=True)
-            for i in range(0, 1500, 10):
-                mdl.train(10)
-                print('Iteration: {}\tLog-likelihood: {}'.format(i, mdl.ll_per_word))
-
-            # calculate coherence of each scheme
-            for preset in ('u_mass', 'c_uci', 'c_npmi', 'c_v'):
-                coh = tp.coherence.Coherence(mdl, coherence=preset, top_n=top_n_words)
-                avg_coherence = coh.get_score()
-                coherence_per_topic = [coh.get_score(topic_id=k) for k in range(mdl.k)]
-
-                print('==== Coherence : {} ===='.format(preset))
-                print('Average:', avg_coherence, '\nPer Topic:', coherence_per_topic)
-
-                coherence_per_topic = [str(e)[:9] for e in coherence_per_topic]
-                coherence_per_topic = ", ".join(coherence_per_topic)
-
-                fw.write(f'{preset}\t{topic_number + 1}\t{str(avg_coherence)[:9]}\t{coherence_per_topic}\n')
+        fw.write('{}\t{}\t{}\t{}\n'.format("scheme_type", "topic_number", "average coherence", "coherence_dist"))
+        for c in coherence_list:
+            fw.write(f'{c.scheme}\t{c.topic_number}\t{c.average_coherence}\t{c.coherence_of_each_topic}\n')
 
 
 if __name__ == '__main__':
 
     start = time.time()  # 시작 시간 저장
 
-    max_num_of_topic = 30
-    top_n_words = 20
+    config = ModelConfig(min_cf=1,
+                         remove_top_n_words=5,
+                         top_n_words=20,
+                         top_n_topic_labels=5)
+
 
     input_file = './data/abstract.txt'
+    max_num_of_topic = 30
     coherence = './coherence_max_' + str(max_num_of_topic) + '.txt'
 
     # load documents on memory
@@ -80,8 +119,8 @@ if __name__ == '__main__':
 
     calculate_coherence(text_data=docs,
                         max_topic_number=max_num_of_topic,
-                        top_n_words=top_n_words,
-                        coherence_path=coherence)
+                        coherence_path=coherence,
+                        model_config=config)
 
     second = time.time() - start
     print("{}시간 ({}분)".format((second/3600), (second/60)))
